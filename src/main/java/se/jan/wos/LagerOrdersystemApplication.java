12fa8ef753b6d5013ea1d9a86c8fb9b6d07bbdbe
Jan Elia <jan.elia995@hotmail.com>
2026-09-05 20:08:53 +0200

    Initial Spring Boot setup with PostgreSQL via Docker Compose

---
+++ src/main/java/se/jan/wos/LagerOrdersystemApplication.java
@@ -0,0 +1,13 @@
+package se.jan.wos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagerOrdersystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagerOrdersystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jan/wos/LagerOrdersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jan.wos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagerOrdersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
